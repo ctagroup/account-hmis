9b448d3c44ad07c9689fdfc864b7ebb66f1721ad
Udit Desai <desaiuditd@gmail.com>
2016-04-03 13:20:33 -0700

    updates version dependencies and release

--- package.js
+++ package.js
@@ -5,16 +5,16 @@ Package.describe(
 	{
 		name: "desaiuditd:accounts-hmis",
 		summary: "Login service for HMIS accounts",
-		version: "0.0.1",
+		version: "0.0.2",
 		git: "https://github.com/desaiuditd/account-hmis",
 	}
 );
 
 Package.onUse(function(api) {
-	api.use('accounts-base@1.2.4', ['client', 'server']);
+	api.use('accounts-base@1.2.2', ['client', 'server']);
 	// Export Accounts (etc) to packages using this one.
-	api.imply('accounts-base@1.2.4', ['client', 'server']);
-	api.use('accounts-oauth@1.1.10', ['client', 'server']);
+	api.imply('accounts-base@1.2.2', ['client', 'server']);
+	api.use('accounts-oauth@1.1.8', ['client', 'server']);
 	api.use('desaiuditd:hmis@0.0.1', ['client', 'server']);
 
 	api.addFiles('hmis_login_button.css', 'client');
